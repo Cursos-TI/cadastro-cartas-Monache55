b7ba6eb579a9243b15c7c705bc1a9285d49e00ae
Monache55 <hectororlandosuarezhernandez55@gmail.com>
2025-10-22 16:31:17 +0000

    novo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,34 +1,48 @@
 #include <stdio.h>
+#include <locale.h>
 
-int main(){
-    
-    int carta, populacao , polos ;
-    float area , pib ;
-    char estado[50] , codigo , cidade ;
-    
-    printf("Digite el numero de su carta:  \n" );
+int main() {
+    setlocale(LC_ALL, "");
+
+    int carta, populacao, polos;
+    float area, pib, densidade;
+    char estado[50], cidade[50];
+
+    printf("Digite el numero de su carta:  \n");
     scanf("%d", &carta);
-    printf("Digite la letra de su Estado:  \n" );
-    scanf("%s", &estado);
-    printf("Digite el nombre de la Ciudad:  \n" );
-    scanf("%s", &cidade);
-    printf("Digite la Poblacion de la Ciudad:  \n" );
+
+    printf("Digite la letra de su Estado:  \n");
+    scanf("%s", estado);
+
+    printf("Digite el nombre de la Ciudad:  \n");
+    scanf(" %49[^\n]", cidade);
+
+    printf("Digite la Poblacion de la Ciudad:  \n");
     scanf("%d", &populacao);
-    printf("Digita el valor del area de la ciudad:  \n" );
+
+    printf("Digita el valor del area de la ciudad:  \n");
     scanf("%f", &area);
-    printf("Digita el valor del PIB:  \n" );
+
+    printf("Digita el valor del PIB:  \n");
     scanf("%f", &pib);
-    printf("Digita la cantidad de polos:  \n" );
+
+    printf("Digita la cantidad de polos:  \n");
     scanf("%d", &polos);
 
+    if (area != 0)
+        densidade = populacao / area;
+    else
+        densidade = 0;
 
-    printf("El numero de tu Carta es : %d\n", carta);
-    printf("La letra de tu Estado es : %s\n", estado);
-    printf("El Codigo de tu Carta es : %s0%d\n", estado, carta);
-    printf("El Nombre de la Ciudad es : %s\n", cidade);
-    printf("La Poblacion de la Ciudad es : %d\n", populacao);
-    printf("El valor del PIB es : %f\n", pib);
-    printf("La Cantidad de polos Turisticos es : %d\n", polos);
+    printf("\n===== RESULTADOS =====\n");
+    printf("El numero de tu Carta es: %d\n", carta);
+    printf("La letra de tu Estado es: %s\n", estado);
+    printf("El Codigo de tu Carta es: %s%02d\n", estado, carta);
+    printf("El Nombre de la Ciudad es: %s\n", cidade);
+    printf("La Poblacion de la Ciudad es: %d\n", populacao);
+    printf("El valor del PIB es: %.2f\n", pib);
+    printf("La Cantidad de polos Turisticos es: %d\n", polos);
+    printf("La Densidad Poblacional es: %.2f\n", densidade);
 
-    return 0 ;
-}   
+    return 0;
+}
